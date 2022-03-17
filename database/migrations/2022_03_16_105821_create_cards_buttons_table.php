9fa05803a06db5097a41ab0888c9ea0865322f00
Marcio Souza <marciooliveira10@gmail.com>
2022-03-17 15:41:00 +0000

    feat: DataBase connection, created container docker.

--- database/migrations/2022_03_16_105821_create_cards_buttons_table.php
+++ database/migrations/2022_03_16_105821_create_cards_buttons_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('card_buttons', function (Blueprint $table) {
+        Schema::create('cards_buttons', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('card_id');
             $table->foreign('card_id')->references('id')->on('cards');
             $table->unsignedBigInteger('button_type_id');
-            $table->foreign('button_type_id')->references('id')->on('buttonTypes');
+            $table->foreign('button_type_id')->references('id')->on('button_types');
             $table->enum('button_size', ['small', 'large', 'extra_large']);
             $table->integer('position');
             $table->string('button_text');
